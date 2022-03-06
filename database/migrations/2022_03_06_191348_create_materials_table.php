289a797df9a193df1356567bf4be002bfcc52138
Aleksei <Alex_somkin@mail.ru>
2022-03-06 22:30:19 +0300

    feat(dev): Add migrations books, sites, materials and videos

---
+++ database/migrations/2022_03_06_191348_create_materials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable(false);
+            $table->string('image')->nullable(false);
+            $table->string('file')->nullable(false);
+            $table->bigInteger('topic_id')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
